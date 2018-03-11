f570000b95ef7f778b17e4bd1d66d702ae0d3106
Taras Shpachenko <taras.shpachenko@gmail.com>
2018-03-11 14:55:58 +0200

    Use bionic gem.

--- Gemfile
+++ Gemfile
@@ -5,17 +5,13 @@ source 'https://rubygems.org'
 ruby '2.5.0'
 
 #=== CORE ==============================================================================================================
-gem 'telegram-bot-ruby', require: 'telegram/bot'
-gem 'activerecord'
-gem 'i18n'
+gem 'bionic'
 gem 'rake'
 gem 'pg',                                                                                                      '~> 0.18'
 
 #=== FEATURES ==========================================================================================================
-gem 'dotenv', require: 'dotenv/load'
 gem 'faraday'
 gem 'nokogiri'
-gem 'standalone_migrations'
 
 group :development do
   gem 'awesome_print'
--- config/application.rb
+++ config/application.rb
@@ -1,31 +1,11 @@
 # frozen_string_literal: true
 
-require 'rubygems'
-require 'bundler'
-require './config/environment'
-Bundler.require(:default, Environment.current)
-
-require './services/base_service'
-require './handlers/base_handler'
-require './services/fetchers/base_fetcher'
-
-Dir['./config/initializers/**/*.rb',
-    './handlers/**/*.rb',
-    './services/**/*.rb',
-    './models/**/*.rb'].sort.each { |file| require file }
+require_relative 'boot'
+require 'bionic'
+Bundler.require(*Bionic.groups)
 
 STDOUT.sync = true
 
-class Application
-  class << self
-    def run
-      Initializers::Database.call
-      Initializers::Localization.call
-      Initializers::Bot.call
-    end
-
-    def logger
-      @logger ||= Logger.new(STDOUT, Logger::DEBUG)
-    end
-  end
+class StarlingBot < Bionic::Application
+  config.locale = :ua
 end
