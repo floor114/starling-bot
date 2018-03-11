00a590d0163ba08cafb80dac8306cc854fb30e87
Taras Shpachenko <taras.shpachenko@onapp.com>
2018-03-11 13:46:35 +0200

    Use bionic gem.

---
+++ app/handlers/base_handler.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Handlers
+  class BaseHandler < Bionic::Handler
+    def call
+      perform_action
+      super
+    end
+
+    private
+
+    attr_reader :message, :bot, :args
+
+    def user
+      @user ||= ::Fetchers::User.call(message.from)
+    end
+
+    def chat
+      @user ||= ::Fetchers::Chat.call(message.chat)
+    end
+
+    def perform_action; end
+  end
+end
