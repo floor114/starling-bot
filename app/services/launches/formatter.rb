b5162ca86a6d09dbcd8816200c5ef44dde906acb
Taras Shpachenko <taras.shpachenko@gmail.com>
2019-11-18 13:06:40 +0200

    Hotfix.

--- app/services/launches/formatter.rb
+++ app/services/launches/formatter.rb
@@ -8,19 +8,10 @@ module Lunches
     end
 
     def call
-      data.each_with_object([]) do |(member, meals), orders|
-        meals = meals.map { |meal| I18n.t('lunch.meal', meal: meal) }.join("\n")
-
-        orders.push("-> #{member}:\n#{meals}")
-      end.join("\n\n")
-    end
-
-    def data
-      @data ||= members.each_with_object(Hash.new([])) do |member, hash|
+      members.each_with_object([]) do |member, orders|
         meals = select_meals(member)
-
-        hash[member] = hash[member].concat(meals) if meals.present?
-      end
+        orders.push("-> #{member}:\n#{meals}") if meals.present?
+      end.join("\n\n")
     end
 
     private
@@ -33,8 +24,8 @@ module Lunches
 
     def select_meals(member)
       parsed_data.each_with_object([]) do |data, meals|
-        meals << data[:meal].downcase if data[:members].any? { |e| e.downcase[member.downcase] }
-      end
+        meals << "* #{data[:meal].downcase};" if data[:members].any? { |e| e.downcase[member.downcase] }
+      end.join("\n")
     end
   end
 end
